3f35cc61b37c768191cdcbe239745c8210396baf
kimsoyeong <iloveit356@gmail.com>
2022-12-26 23:58:49 +0900

    Create Ticket(svg)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { ReactComponent as Temp } from "./images/Ticket.svg";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("SOYEONG KIM");
+
+  useEffect(() => {
+    let svg = document.getElementsByTagNameNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    )[0];
+    let temp = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    temp.setAttribute("x", 30);
+    temp.setAttribute("y", 180);
+    temp.style.fill = "black";
+    temp.style.font = "bold 20px Inter";
+    temp.innerHTML = name;
+
+    svg.appendChild(temp);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Temp fill="#F1B0F4" />
+        {/* orange: #F1B000 */}
       </header>
     </div>
   );
